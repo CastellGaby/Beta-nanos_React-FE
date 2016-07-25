0a5331a2e75cc93f2e91e01f07f64aef7923745a
CastellGaby <castellgaby.dubon@unitec.edu>
2016-07-25 13:44:52 -0600

    HTML/React - GC/LI

---
+++ src/music.js
@@ -0,0 +1,35 @@
+
+var Note = React.createClass({
+    edit: function() {
+        alert('editing note'+ document.getElementById('title_input').value);
+    },
+    remove: function() {
+        alert('removing note');
+    },
+    render: function() {
+        return (
+            <div className="note">
+                <form>
+                <div>
+                    <label>Tittle</label>
+                    <input type="text" id="title_input"/>
+                </div>
+                <div>
+                    <label>Price</label>
+                    <input type="text"/>
+                </div>
+                <div>
+                    <label>Rating</label>
+                    <input type="text"/>
+                </div>
+                <button type="submit" className="btn btn-primary" onClick={this.edit}>CREATE</button>
+                </form>
+            </div>
+            );
+    }
+});
+React.render(<div>
+    <Note/>
+    </div>, 
+    document.getElementById('create'));
+
